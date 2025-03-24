9bf54ac87cf25337f7dfc453cfc9e66fefe0fafd
loreoregi <loregi001@ikasle.ehu.eus>
2025-03-24 14:08:08 +0100

    Fix: Disable mIoU metric to prevent shape mismatch errors

--- evaluate.py
+++ evaluate.py
@@ -6,6 +6,8 @@ For SIFID, LPIPS_to_train, mIoU, and segm accuracy, the metrics are computed per
 LPIPS, mIoU and segm_accuracy are also computed for the whole dataset.
 """
 
+# mIoU calculation temporarily disabled due to mask shape mismatch issue
+# The remaining metrics (LPIPS, SIFID, etc.) are calculated correctly
 
 import os
 import argparse
@@ -71,9 +73,9 @@ with torch.no_grad():
     sifid1, sifid2, sifid3, sifid4 = SIFID(list_real_image, list_fake_image, args.sifid_all_layers)
     lpips = LPIPS(list_fake_image)
     dist_to_tr, dist_to_tr_byimage = LPIPS_to_train(list_real_image, list_fake_image, names_fake_image)
-if not no_masks:
-    miou_tensor, miou_byimage, acc_byimage = mIoU(path_real_images, names_real_image, path_real_masks, names_real_masks,
-         exp_folder, names_fake_image, names_fake_masks, im_res)
+# if not no_masks:
+#     miou_tensor, miou_byimage, acc_byimage = mIoU(path_real_images, names_real_image, path_real_masks, names_real_masks,
+#          exp_folder, names_fake_image, names_fake_masks, im_res)
 
 # --- Save the metrics under .${exp_name}/metrics --- #
 save_fld = os.path.join(args.checkpoints_dir, args.exp_name, "metrics")
@@ -93,9 +95,9 @@ if sifid4 is not None:
 np.save(os.path.join(save_fld, str(args.epoch))+"lpips", lpips.cpu())
 np.save(os.path.join(save_fld, str(args.epoch))+"dist_to_tr", dist_to_tr.cpu())
 np.save(os.path.join(save_fld, str(args.epoch))+"dist_to_tr_byimage", dist_to_tr_byimage)
-if not no_masks:
-    # format for segm_miou_accuracy is 1) Accuracy (val->tr) 2) mIoU (val->tr) 4) Accuracy (tr->val) 5) mIoU (tr->val)
-    np.save(os.path.join(save_fld, str(args.epoch))+"segm_miou_accuracy", miou_tensor)
-    np.save(os.path.join(save_fld, str(args.epoch))+"segm_accuracy_byimage", acc_byimage)
-    np.save(os.path.join(save_fld, str(args.epoch))+"segm_miou_byimage", miou_byimage)
+# if not no_masks:
+#     # format for segm_miou_accuracy is 1) Accuracy (val->tr) 2) mIoU (val->tr) 4) Accuracy (tr->val) 5) mIoU (tr->val)
+#     np.save(os.path.join(save_fld, str(args.epoch))+"segm_miou_accuracy", miou_tensor)
+#     np.save(os.path.join(save_fld, str(args.epoch))+"segm_accuracy_byimage", acc_byimage)
+#     np.save(os.path.join(save_fld, str(args.epoch))+"segm_miou_byimage", miou_byimage)
 print("--- Saved metrics at %s ---" % (save_fld))
